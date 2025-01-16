07982e1c2b83b7545c167b611067c1247c5778aa
Nurzhan Kozhanov <nurzhan.k@lightsoul.dev>
2025-01-16 14:52:11 +0500

    fix: better deck descr

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -4,8 +4,10 @@ return {
             b_random_deck_random_deck = {
                 name = "Random Deck",
                 text = {
-                    "All cards are {C:attention}Randomized after scoring.",
-                    "Start with a random deck of cards.",
+                    "All cards are",
+                    "{C:attention}Randomized{} after scoring.",
+                    "Start with a",
+                    "{C:enhanced}Randomized{} deck.",
                 },
             },
         },
